92975aeb40821655133d2950380ad398372e9c91
David Gonçalves Maia <david.goncalves@dcx.ufpb.br>
2023-11-26 22:12:55 -0300

    Mais um Exercício...

---
+++ src/exercicios/two/Rectangle.java
@@ -0,0 +1,25 @@
+package exercicios.two;
+
+public class Rectangle {
+    public double width;
+    public double height;
+
+
+    public double area(double width, double height){
+        return width * height;
+    }
+
+    public double perimeter(double height, double width){
+        return 2 * (height + width);
+    }
+
+    public double diagonal(double width, double height){
+        return Math.sqrt(Math.pow(height, 2) + Math.pow(width, 2));
+    }
+
+    public String toString(){
+        return "AREA = " + String.format("%.2f",area(width, height)) + "\n" +
+        "PERIMETER = " + String.format("%.2f",perimeter(width, height)) + "\n" +
+                "DIAGONAL = " + String.format("%.2f", diagonal(width, height));
+    }
+}
---
+++ src/exercicios/two/TestRectangle.java
@@ -0,0 +1,25 @@
+package exercicios.two;
+
+import java.util.Scanner;
+
+public class TestRectangle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Rectangle rec = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+
+        rec.width = sc.nextDouble();
+        rec.height = sc.nextDouble();
+
+        System.out.println(rec.toString());
+
+
+
+
+
+
+
+    }
+}
